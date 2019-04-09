2afcce377507ee1c57addb3181b6eb1460235e19
Marcos Kawakami <marcoskwkm@gmail.com>
2019-04-09 13:21:04 -0300

    Add VTEX abbreviations and yarn

--- fish/functions/setup.fish
+++ fish/functions/setup.fish
@@ -5,4 +5,10 @@ function setup
     end
     abbr gg git graph
     abbr gs git status
+    abbr prod vtex config set env prod
+    abbr staging vtex config set env staging
+    abbr vl vtex link --verbose --unsafe
+    abbr vls vtex ls
+    abbr vwr vtex workspace reset
+    abbr vu vtex unlink    
 end
