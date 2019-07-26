89ac9d693495856ad66794e20feee04ca1fe08ac
Marcos Kawakami <marcos.kawakami@vtex.com.br>
2019-07-26 10:52:07 -0300

    Fix bug in get-app script

--- fish/functions/scripts/get-app-script.py
+++ fish/functions/scripts/get-app-script.py
@@ -1,2 +1,2 @@
 path = raw_input().split('/')
-print '/' + path[path.index('vtex') + 1] if 'vtex' in path else ''
+print '/' + path[path.index('vtex') + 1] if 'vtex' in path and 'vtex' != path[-1] else ''
