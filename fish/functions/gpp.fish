c8aba2358470a6edd364a1796f1b4a464884637f
Marcos Kawakami <marcoskwkm@gmail.com>
2019-09-14 13:00:23 -0300

    Use gcc-9 in fish

--- fish/functions/gpp.fish
+++ fish/functions/gpp.fish
@@ -1,3 +1,3 @@
 function gpp --description 'Runs g++ with usual competitive programming arguments'
-    g++ $argv.cpp -std=c++11 -Wall -O2 -g -o $argv
+    g++-9 $argv.cpp -std=c++11 -Wall -O2 -g -o $argv
 end
