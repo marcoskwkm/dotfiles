f608d99c624f2cbf482c2e4a1ca015daf40b35c5
Marcos Kawakami <marcos.kawakami@vtex.com.br>
2019-10-02 12:02:20 -0300

    [Emacs] Enable flycheck-mode in json-mode

--- emacs/config/json.el
+++ emacs/config/json.el
@@ -2,3 +2,10 @@
 
 ;; Makes json-mode derive from prog-mode
 (put 'json-mode 'derived-mode-parent 'prog-mode)
+
+;; Enables flycheck
+(defun setup-flycheck ()
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled)))
+(when (fboundp 'typescript-mode)
+  (add-hook 'json-mode-hook 'setup-flycheck))
