d7a6a3caad6f8da59184993e9c94454a59ac507b
Marcos Kawakami <marcos.kawakami@vtex.com.br>
2020-02-22 16:02:34 -0300

    [emacs] Fix indentation of lambda functions in C++

--- emacs/config/c.el
+++ emacs/config/c.el
@@ -2,16 +2,19 @@
 (setq c-default-style "linux")
 (c-set-offset 'case-label '+) ; indents switch-case block
 
-;; For fixing indentation with C++ lambda functions
-(defadvice c-lineup-arglist (around my activate)
-  "Improve indentation of continued C++11 lambda function opened as argument."
-  (setq ad-return-value
-        (if (and (equal major-mode 'c++-mode)
-                 (ignore-errors
-                   (save-excursion
-                     (goto-char (c-langelem-pos langelem))
-                     ;; Detect "[...](" or "[...]{". preceded by "," or "(",
-                     ;;   and with unclosed brace.
-                     (looking-at ".*[(,][ \t]*\\[[^]]*\\][ \t]*[({][^}]*$"))))
-            0                           ; no additional indent
-          ad-do-it)))                   ; default behavior
+;; ;; For fixing indentation with C++ lambda functions (Emacs25 or belo)
+;; (defadvice c-lineup-arglist (around my activate)
+;;   "Improve indentation of continued C++11 lambda function opened as argument."
+;;   (setq ad-return-value
+;;         (if (and (equal major-mode 'c++-mode)
+;;                  (ignore-errors
+;;                    (save-excursion
+;;                      (goto-char (c-langelem-pos langelem))
+;;                      ;; Detect "[...](" or "[...]{". preceded by "," or "(",
+;;                      ;;   and with unclosed brace.
+;;                      (looking-at ".*[(,][ \t]*\\[[^]]*\\][ \t]*[({][^}]*$"))))
+;;             0                           ; no additional indent
+;;           ad-do-it)))                   ; default behavior
+
+;; Fixes lambda indentation in Emacs26
+(c-set-offset 'inlambda '0)
