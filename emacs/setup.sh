0686a7d9eab24b6eb1eee3ca98f9ecb85560f1c2
Marcos Kawakami <marcoskwkm@gmail.com>
2021-04-25 03:32:05 -0300

    [Emacs] Fix setup

--- emacs/setup.sh
+++ emacs/setup.sh
@@ -7,12 +7,13 @@ cd "$DIR"
 
 CONFIG_SRC="$(realpath .)"
 CONFIG_DST="$(realpath ~/.emacs.d)"
+HOME="$(realpath ~/)"
 
 info "Configuring emacs..."
 mkdir -p "$CONFIG_DST"
 mkdir -p "$CONFIG_DST/config"
 
-symlink "$CONFIG_SRC/.clang-format" "$(realpath ~/.clang-format)"
+symlink "$CONFIG_SRC/.clang-format" "$HOME/.clang-format"
 
 find . -name "*.el" | while read fn; do
     symlink "$CONFIG_SRC/$fn" "$CONFIG_DST/$fn"
