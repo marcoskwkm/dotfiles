ec8c1b2bfea5ef1a900ea366862135f5eeb96787
Marcos Kawakami <marcoskwkm@gmail.com>
2021-05-08 18:25:50 -0300

    [Emacs] Improve C++ setup

--- emacs/config/cpp.el
+++ emacs/config/cpp.el
@@ -1,4 +1,18 @@
+(require 'flycheck)
+
+(setq-default flycheck-c/c++-gcc-executable "/usr/local/bin/gcc-9")
+(setq-default flycheck-gcc-language-standard "c++17")
+(setq-default flycheck-disabled-checkers '(c/c++-clang))
+
+(defun setup-clang-formatter ()
+  (add-hook (make-local-variable 'before-save-hook)
+            'clang-format-buffer))
+
+(defun setup-flycheck ()
+  (flycheck-mode +1)
+  (flycheck-select-checker 'c/c++-gcc))
+
 (add-hook 'c++-mode-hook
           (lambda ()
-            (add-hook (make-local-variable 'before-save-hook)
-                      'clang-format-buffer)))
+            (setup-clang-formatter)
+            (setup-flycheck)))
