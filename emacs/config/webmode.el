859e73ff0d9677dcd69fd05592dbd241d83c588a
Marcos Kawakami <marcoskwkm@gmail.com>
2021-06-11 16:58:51 -0300

    [Emacs] Use javascript-eslint as checker in webmode

--- emacs/config/webmode.el
+++ emacs/config/webmode.el
@@ -1,3 +1,4 @@
+(require 'flycheck)
 (require 'tide-setup)
 
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
@@ -28,10 +29,33 @@
 (add-hook 'web-mode-hook #'(lambda ()
                              (enable-minor-mode
                               '("\\.(jsx?|tsx?)\\'" . prettier-js-mode))))
+
+(defun lunaryorn-use-js-executables-from-node-modules ()
+  "Set executables of JS checkers from local node modules."
+  (-when-let* ((file-name (buffer-file-name))
+               (root (locate-dominating-file file-name "node_modules"))
+               (module-directory (expand-file-name "node_modules" root)))
+    (pcase-dolist (`(,checker . ,module) '((javascript-jshint . "jshint")
+                                           (javascript-eslint . "eslint")
+                                           (javascript-jscs   . "jscs")))
+      (let ((package-directory (expand-file-name module module-directory))
+            (executable-var (flycheck-checker-executable-variable checker)))
+        (when (file-directory-p package-directory)
+          (set (make-local-variable executable-var)
+               (expand-file-name (concat "bin/" module ".js")
+                                 package-directory)))))))
+
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+
+(defun enable-flycheck ()
+  (lunaryorn-use-js-executables-from-node-modules)
+  (flycheck-select-checker 'javascript-eslint)
+  (flycheck-mode))
+
 (eval-after-load 'web-mode
   '(progn
      (add-hook 'web-mode-hook 'company-mode)
      (add-hook 'web-mode-hook #'add-node-modules-path)
      (add-hook 'web-mode-hook #'prettier-js-mode)
      (add-hook 'web-mode-hook 'hs-minor-mode) ;; Activates hide-show mode (for folding)
-     (add-hook 'web-mode-hook 'flycheck-mode)))
+     (add-hook 'web-mode-hook 'enable-flycheck)))
