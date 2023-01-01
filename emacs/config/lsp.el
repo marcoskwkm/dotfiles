f2fe953a45e278cb4ac1fb4b56d3f9a6b2e6a8f6
Marcos Kawakami <marcoskwkm@gmail.com>
2022-12-31 21:21:39 -0300

    Update emacs config

--- emacs/config/lsp.el
+++ emacs/config/lsp.el
@@ -1,3 +1,4 @@
 (require 'lsp)
 
 (setq-default lsp-ui-doc-position 'at-point)
+(setq lsp-ui-doc-show-with-mouse nil)
---
+++ emacs/config/tabbar.el
@@ -0,0 +1,5 @@
+(tab-bar-mode 1)
+(global-set-key (kbd "s-{") 'tab-previous)
+(global-set-key (kbd "s-}") 'tab-next)
+(global-set-key (kbd "C-s-n") 'tab-new)
+(global-set-key (kbd "C-s-w") 'tab-close)
