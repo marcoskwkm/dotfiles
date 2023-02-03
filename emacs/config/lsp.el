21415170c60d4d0377bda389c04f7a4b00d394bc
Marcos Kawakami <marcoskwkm@gmail.com>
2023-02-03 04:52:46 -0300

    [Emacs] Change M-. keybind to use peek definition instead in LSP mode

--- emacs/config/lsp.el
+++ emacs/config/lsp.el
@@ -2,3 +2,6 @@
 
 (setq-default lsp-ui-doc-position 'at-point)
 (setq lsp-ui-doc-show-with-mouse nil)
+
+(add-hook 'lsp-configure-hook (lambda ()
+                                (local-set-key (kbd "M-.") 'lsp-ui-peek-find-definitions)))
