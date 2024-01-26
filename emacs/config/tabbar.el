d483650d46167606e8064fdcc4a78e6e23647dce
Marcos Kawakami <marcoskwkm@gmail.com>
2024-01-26 19:33:02 -0300

    [Emacs] Use tab-bar instead of tab-line

--- emacs/config/tabbar.el
+++ emacs/config/tabbar.el
@@ -1,7 +1,5 @@
-;; Disabled to favor Tab Line mode instead
-;;
-;; (tab-bar-mode 1)
-;; (global-set-key (kbd "s-{") 'tab-previous)
-;; (global-set-key (kbd "s-}") 'tab-next)
-;; (global-set-key (kbd "C-s-n") 'tab-new)
-;; (global-set-key (kbd "C-s-w") 'tab-close)
+(tab-bar-mode 1)
+(global-set-key (kbd "s-{") 'tab-previous)
+(global-set-key (kbd "s-}") 'tab-next)
+(global-set-key (kbd "C-s-n") 'tab-new)
+(global-set-key (kbd "C-s-w") 'tab-close)
--- emacs/config/tabline.el
+++ emacs/config/tabline.el
@@ -1,6 +1,8 @@
-[](global-tab-line-mode t)
-(global-set-key (kbd "s-{") 'previous-buffer)
-(global-set-key (kbd "s-}") 'next-buffer)
+;; Disabled in favor of tab-bar mode
+;;
+;; (global-tab-line-mode t)
+;; (global-set-key (kbd "s-{") 'previous-buffer)
+;; (global-set-key (kbd "s-}") 'next-buffer)
 
-(custom-set-faces
- '(tab-line ((t (:inherit variable-pitch :background "grey85" :foreground "black" :height 1)))))
+;; (custom-set-faces
+;;  '(tab-line ((t (:inherit variable-pitch :background "grey85" :foreground "black" :height 1)))))
